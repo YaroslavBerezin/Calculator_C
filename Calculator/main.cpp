7b0f2bad0ccafd41f22be8f10b7d19a7b402d0cb
yaroslavberezin <yb2003work@gmail.com>
2022-02-20 22:38:01 +0300

    Initial Commit

---
+++ Calculator/main.cpp
@@ -0,0 +1,51 @@
+//
+//  main.cpp
+//  Calculator
+//
+//  Created by Yaroslav Berezin on 20.02.2022.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+double calculateNumbersAndReturnAnswer(double number1, double number2, char operation) {
+    switch (operation) {
+        case '+':
+            return number1 + number2;
+        case '-':
+            return number1 - number2;
+        case '*':
+            return number1 * number2;
+        case '/':
+            return number1 / number2;
+        default:
+            return 0;
+    }
+}
+
+double readDouble(int serial) {
+    cout << "Input the " << serial << " number (integer): ";
+    double number;
+    cin >> number;
+    return number;
+}
+
+char readChar() {
+    cout << "Input a character (+, -, *, /, %): ";
+    char operation;
+    cin >> operation;
+    return operation;
+}
+
+
+int main() {
+    double number1 = readDouble(1);
+    double number2 = readDouble(2);
+    char operation = readChar();
+    
+    cout << "Answer: " << calculateNumbersAndReturnAnswer(number1, number2, operation) << endl;
+    
+    return 0;
+}
